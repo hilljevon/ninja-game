64c7a38274dade232fad94587bef1e69107f0f69
Jevon Hill <jevon623@gmail.com>
2024-03-18 22:30:53 -0700

    Tile implementation. Create player gravity system

--- game.py
+++ game.py
@@ -1,28 +1,33 @@
 import pygame
 import sys
 from scripts.entities import PhysicsEntity
-from scripts.utils import load_image
+from scripts.utils import load_image, load_images
+from scripts.tilemap import Tilemap
 class Game:
     def __init__(self) -> None: 
         pygame.init()
         pygame.display.set_caption('ninja game')
         self.screen = pygame.display.set_mode((640, 480))
-        # pygame.Surface renders a blank surface image
         self.display = pygame.Surface((320, 240))
         self.clock = pygame.time.Clock()
         self.movement = [False, False]
         self.player = PhysicsEntity(self, 'player', (50, 50), (8, 15))
         self.assets = {
-            'player': load_image('entities/player.png')
+            'player': load_image('entities/player.png'),
+            'decor': load_images('tiles/decor'),
+            'grass': load_images('tiles/grass'),
+            'large_decor': load_images('tiles/large_decor'),
+            'spawners': load_images('tiles/spawners'),
+            'stone': load_images('tiles/stone'),
         }
-
+        self.tile_map = Tilemap(self, 16)    
+        self.scroll = [0,0]
     def run(self):
         while True:
             self.display.fill((14, 219 ,248))
-            
-            self.player.update((self.movement[1] - self.movement[0], 0))
+            self.tile_map.render(self.display)
+            self.player.update(self.tile_map,(self.movement[1] - self.movement[0], 0))
             self.player.render(self.display)
-
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
@@ -32,6 +37,8 @@ class Game:
                         self.movement[0] = True
                     if event.key == pygame.K_RIGHT:
                         self.movement[1] = True
+                    if event.key == pygame.K_UP:
+                        self.player.velocity[1] = -3
                 if event.type == pygame.KEYUP:
                     if event.key == pygame.K_LEFT:
                         self.movement[0] = False
--- scripts/utils.py
+++ scripts/utils.py
@@ -1,7 +1,15 @@
 import pygame
+import os
 BASE_IMG_PATH = 'data/images/'
 def load_image(path):
     # .convert allows for performance
     img = pygame.image.load(BASE_IMG_PATH + path).convert()
     img.set_colorkey((0,0,0))
     return img
+
+def load_images(path):
+    images = []
+    # os.listdir takes a path as argument and returns all images in that path. in this case, navigating to our specified pathing and iterating through the entire directory
+    for img_name in os.listdir(BASE_IMG_PATH + path):
+        images.append(load_image(path + '/' + img_name))
+    return images
