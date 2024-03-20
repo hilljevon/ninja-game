bb949e86820ba17da71ea179bbb26213646e2609
Jevon Hill <jevon623@gmail.com>
2024-03-20 14:01:21 -0700

    Added clouds + camera movement.

--- scripts/tilemap.py
+++ scripts/tilemap.py
@@ -37,13 +37,13 @@ class Tilemap:
         for tile in self.offgrid_tiles:
             surf.blit(
                 self.game.assets[tile['type']][tile['variant']], 
-                      tile['pos']
+                     ( tile['pos'][0] - offset[0], tile['pos'][1] - offset[1] )
             ) 
         for location in self.tilemap:
             tile = self.tilemap[location]
             surf.blit(
                     self.game.assets[tile['type']][tile['variant']], 
-                      (tile['pos'][0] * self.tile_size,
-                      tile['pos'][1] * self.tile_size)
+                      (tile['pos'][0] * self.tile_size - offset[0],
+                      tile['pos'][1] * self.tile_size - offset[1])
                       )
         
