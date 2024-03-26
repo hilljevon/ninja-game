40f49629a0cc71d4ce9aa263d33724bad371f1db
Jevon Hill <jevon623@gmail.com>
2024-03-26 12:59:18 -0700

    Created enemies, rendered onto screen with PhysicsEntity class inheritance. Created function to prevent enemy from automatically walking off the platform.

--- scripts/entities.py
+++ scripts/entities.py
@@ -3,7 +3,7 @@ import random
 
 import pygame
 
-from scripts.particles import Particle
+from scripts.particle import Particle
 
 class PhysicsEntity:
     def __init__(self, game, e_type, pos, size):
@@ -74,7 +74,25 @@ class PhysicsEntity:
         
     def render(self, surf, offset=(0, 0)):
         surf.blit(pygame.transform.flip(self.animation.img(), self.flip, False), (self.pos[0] - offset[0] + self.anim_offset[0], self.pos[1] - offset[1] + self.anim_offset[1]))
+class Enemy(PhysicsEntity):
+    def __init__(self, game, pos, size):
+        super().__init__(game, 'enemy', pos, size)
 
+        self.walking = 0
+    # update function serves to either keep an enemy moving in that direction or if there is no solid tile in front of them, turn around
+    def update(self, tilemap, movement=(0,0)):
+        if self.walking:
+            # this line checks right in front of them to make sure there is a solid tile in front of the enemy
+            if tilemap.solid_check((self.rect().centerx + (-7 if self.flip else 7), self.pos[1] + 23 )):
+                movement = (movement[0] - 0.5 if self.flip else 0.5, movement[1]) 
+            else:
+                self.flip = not self.flip
+            self.walking = max(0, self.walking - 1) 
+        elif random.random() < 0.01:
+            self.walking = random.randint(30, 120)
+        super().update(tilemap, movement=movement)
+
+        
 class Player(PhysicsEntity):
     def __init__(self, game, pos, size):
         super().__init__(game, 'player', pos, size)
