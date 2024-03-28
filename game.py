a3a3e928e55abd43b91be4bb3832ffc981d4534b
Jevon Hill <jevon623@gmail.com>
2024-03-27 17:03:53 -0700

    Created transition at beginning and end of level.

--- game.py
+++ game.py
@@ -46,11 +46,12 @@ class Game:
         self.clouds = Clouds(self.assets['clouds'], count=16)
         
         self.player = Player(self, (50, 50), (8, 15))
-        
+        self.level = 0
         self.tilemap = Tilemap(self, tile_size=16)
-        self.load_level(0)
-
+        self.load_level(self.level)
         
+        self.screenshake = 0
+
         
 
         
@@ -62,7 +63,7 @@ class Game:
         
         self.enemies = []
         for spawner in self.tilemap.extract([('spawners', 0), ('spawners', 1)]):
-            if spawner['variant'] != 0:
+            if spawner['variant'] == 0:
                 self.player.pos = spawner['pos']
                 self.player.air_time = 0 
             else:
@@ -73,16 +74,28 @@ class Game:
         self.sparks = []
         self.scroll = [0, 0]
         self.dead = 0
+        self.transition = -30
 
 
     def run(self):
         while True:
             self.display.blit(self.assets['background'], (0, 0))
             
+            self.screenshake = max(0, self.screenshake - 1)
+            
+            if not len(self.enemies):
+                self.transition +=1 
+                if self.transition > 30:
+                    self.level += 1 
+                    self.load_level(self.level)
+            if self.transition < 0:
+                self.transition += 1
+
+
             if self.dead:
                 self.dead += 1
                 if self.dead > 40:
-                    self.load_level(0)
+                    self.load_level(self.level)
             self.scroll[0] += (self.player.rect().centerx - self.display.get_width() / 2 - self.scroll[0]) / 30
             self.scroll[1] += (self.player.rect().centery - self.display.get_height() / 2 - self.scroll[1]) / 30
             render_scroll = (int(self.scroll[0]), int(self.scroll[1]))
@@ -125,6 +138,7 @@ class Game:
                     if self.player.rect().collidepoint(projectile[0]):
                         self.projectiles.remove(projectile)
                         self.dead += 1
+                        self.screenshake = max(16, self.screenshake)
                         for i in range(30):
                             angle = random.random() * math.pi * 2
                             speed = random.random() * 5
@@ -166,7 +180,14 @@ class Game:
                     if event.key == pygame.K_RIGHT:
                         self.movement[1] = False
             
-            self.screen.blit(pygame.transform.scale(self.display, self.screen.get_size()), (0, 0))
+            if self.transition:
+                transition_surf = pygame.Surface(self.display.get_size())
+                pygame.draw.circle(transition_surf, (255, 255, 255), (self.display.get_width() // 2, self.display.get_height() // 2), (30 - abs(self.transition)) * 8)
+                transition_surf.set_colorkey((255, 255, 255))
+                self.display.blit(transition_surf, (0,0))
+
+            screenshake_offset = (random.random() * self.screenshake - self.screenshake / 2,random.random() * self.screenshake - self.screenshake / 2 )
+            self.screen.blit(pygame.transform.scale(self.display, self.screen.get_size()), screenshake_offset)
             pygame.display.update()
             self.clock.tick(60)
 
